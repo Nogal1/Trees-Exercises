d9d03310d6b16957efba760e229a5f6e1d5d5fc9
Nogal1 <mcclendon.logan1234@gmail.com>
2024-09-08 13:41:51 -0700

    jest installed, binary-tree - implemented passing some tests

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,73 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (!this.root) return 0;
 
+    function minDepthHelper(node) {
+      if (!node) return Infinity;
+      if (!node.left && !node.right) return 1;
+      return 1 + Math.min(minDepthHelper(node.left), minDepthHelper(node.right));
+    }
+
+    return minDepthHelper(this.root);
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (!this.root) return 0;
 
+    function maxDepthHelper(node){
+      if (!node) return 0;
+      return 1 + Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right));
+    }
+
+    return maxDepthHelper(this.root);
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let result = { max: -Infinity};
 
+    function maxPathSumHelper(node) {
+      if (!node) return 0;
+
+      let leftMax = Math.max(0, maxPathSumHelper(node.left));
+      let rightMax = Math.max(0, maxPathSumHelper(node.left));
+
+      result.max = Math.max(result.max, node.val + leftMax + rightMax);
+
+      return node.val + Math.max(leftMax, rightMax);
+    }
+
+    maxPathSumHelper(this.root);
+    return result.max;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
 
+    let closest = null;
+
+    function findNextLargerHelper(node) {
+      if (!node) return;
+
+      if (node.val > lowerBound && (closest === null || node.val < closest)) {
+        closest = node.val;
+      }
+
+      findNextLargerHelper(node.left);
+      findNextLargerHelper(node.right);
+    }
+
+    findNextLargerHelper(this.root);
+    return closest;
   }
 
   /** Further study!
